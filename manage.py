80e33b9f9eccae1e70b4f167382650d9e752b1d4
Marionjames-ally <marionjames2018@gmail.com>
2019-11-25 09:03:27 +0300

    create a shell context

--- manage.py
+++ manage.py
@@ -1,2 +1,8 @@
 from app import create_app,db
 from app.models import User
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,Comment = Comment,Category = Category,Subscribe = Subscribe)
+
