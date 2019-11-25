0a0d6d676b36aa40cd5ac033c0b4c7f63cab6845
Marionjames-ally <marionjames2018@gmail.com>
2019-11-25 09:12:29 +0300

    create config class

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = "elias"
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class DevConfig(Config):
+    
+
+    DEBUG = True
+
+class TestConfig(Config):
+    '''
+    child test configuration class
+    '''
+    pass
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@ from app import create_app,db
 from app.models import User
 
 
+# creating app instance
+app = create_app('production')
+
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User,Comment = Comment,Category = Category,Subscribe = Subscribe)
