56cf63366791da68446c21de5991f81c9e7409eb
Marionjames-ally <marionjames2018@gmail.com>
2019-11-26 16:31:09 +0300

    add routes to open the sign-up form

--- app/auth/views.py
+++ app/auth/views.py
@@ -15,16 +15,16 @@ def login():
         user = User.query.filter_by(username).first()
         if user is None:
             error = 'A user with that username does not exist'
-            return render_template('auth/login.html', error=error)
+            return redirect('auth/login.html', error=error)
 
         is_correct_password = user.check_password(password)
         print(is_correct_password)
         if not is_correct_password:
             error = 'A user with that password does not exist'
-            return render_template('login.html',error=error)
+            return redirect('auth/login.html',error=error)
         login_user(user)
         return redirect('/')
-    return render_template('/index.html')
+    return render_template('/auth/login.html')
 
 
       ##sign up content
