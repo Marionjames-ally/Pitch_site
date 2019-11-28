ca6f0ebc72b441a12912ae13befbfffe94cd1cf2
Marionjames-ally <marionjames2018@gmail.com>
2019-11-28 09:08:47 +0300

    deployment

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from app import create_app
 from app import db
 from app.models import User,Post,Comment,DownVote
 
-app = create_app('development')
+# app = create_app('development')
 app = create_app('production')
 
 manager = Manager(app)
